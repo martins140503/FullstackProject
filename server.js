9495dcb8760915aeb776871d74a6a1ff37234de5
Martins <patfinmartins@gmail.com>
2024-11-18 15:34:57 +0000

    remove fs and used post request to database

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const fs = require('fs');
+// const fs = require('fs');
 const { MongoClient } = require('mongodb');
 
 // Create an Express application
